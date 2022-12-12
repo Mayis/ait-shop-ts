871045463b3d7ff88a3015d76eaafd2509b5d9f9
Mayis <saroyan.mayis.ait@gmail.com>
2022-12-12 16:51:11 +0400

    added logout fn ,some style with top sell sldier

--- src/components/Slider/CaruselSlider.tsx
+++ src/components/Slider/CaruselSlider.tsx
@@ -5,12 +5,7 @@ import "./style/style.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 
 import { Autoplay } from "swiper";
-import Card from "@mui/material/Card";
-import { CardActionArea } from "@mui/material";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import { Items } from "../../redux/slices/productSlice";
-import Typography from "@mui/material/Typography";
 
 // MUI
 
@@ -22,37 +17,32 @@ type Props = {
 function CaruselSlider({ items }: Props) {
   return (
     <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={20}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        modules={[Autoplay]}
-        className="mySwiper"
-      >
-        {items?.map((item) => (
-          <SwiperSlide key={item.id}>
-            <Card sx={{ maxWidth: 345 }}>
-              <CardActionArea>
-                <CardMedia
-                  component="img"
-                  height="150"
-                  image={item.src}
-                  alt={item.title}
-                  sx={{ objectFit: "cover" }}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="inherit" component="p">
-                    {item.title}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className="fullSlider">
+        <div className="topSellAnouce">
+          <h1 className="topH1">TOP SELLERS</h1>
+        </div>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={20}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          modules={[Autoplay]}
+          className="mySwiper"
+        >
+          {items?.map((item) => (
+            <SwiperSlide key={item.id}>
+              <div className="imgDiv">
+                <img className="topSellImg" src={item.src} alt={item.title} />
+              </div>
+              <div className="titleDiv">
+                <p className="topSellTitle">${item.title}</p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
     </>
   );
 }
